eb906f4882e127c72a8e6f27ddab68a99ae05b35
Matias Beckerle <matiasbeckerle@gmail.com>
2017-03-11 14:03:33 -0300

    Add testing infrastructure

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.get('/', function (req, res) {
+  res.send('Node API');
+});
+
 app.listen(port, (err) => {
   if (err) return console.log(err);
   console.log(`Server started. Listening on port: ${port}.`);
